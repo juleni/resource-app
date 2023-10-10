19cfc2822549612ce06bd4043eb5f60a2c80f4d6
juleni <69912179+juleni@users.noreply.github.com>
2023-10-10 15:32:23 +0200

    Changed banner url

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -54,7 +54,7 @@ module.exports = {
         orange: "#FF964B",
       },
       backgroundImage: {
-        banner: "url('/jsm_resources_banner.svg')",
+        banner: "url('/resources_banner.svg')",
       },
       keyframes: {
         "accordion-down": {
