2ec936b1723befc2eacf47e4578871b91678cb73
juleni <69912179+juleni@users.noreply.github.com>
2023-10-10 14:05:46 +0200

    Added Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer
+      className="text-white-800 flex-between body-text w-full gap-y-10
+    border-t border-black-400 bg-black-100 px-20 py-1 max-md:flex-col"
+    >
+      <p>2023 Created by JULENI</p>
+      <div className="flex gap-x-9">
+        <Link href="terms-of-use">Terms & Conditions</Link>
+        <Link href="privacy-policy">Privacy Policy</Link>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
