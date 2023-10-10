7c55030f15ad3bdc1c1eddb9d66f92f97e0baf43
juleni <69912179+juleni@users.noreply.github.com>
2023-10-10 11:16:24 +0200

    Moved to (root)/(home) dir

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1>HOME</h1>
+      <h1 className="text-2xl underline">HOME</h1>
       <Button>Click me</Button>
     </div>
   );
